0c6c3e6cc0b33425082cd6c33f8af868816ebe4f
Alena <awff@mail.ru>
2018-09-20 00:53:38 +0300

    log of correspondence

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const net = require('net');
 const fs = require('fs');
 const port = 8124;
-let clientID = Date.now();
+let clientID = 1;
 
 //Перемешивание массива
 function shuffleArray(array) {
@@ -23,29 +23,37 @@ shuffleArray(testArray);
 
 const server = net.createServer((client) => {
   console.log('Client '+clientID +' connected');
-
   client.setEncoding('utf8');
 
   client.on('data', (data) => {
+   
     console.log('client send: ' + data);
-    if(data == 'QA'){      
+    if(data == 'QA'){  
+      fs.writeFile("Client"+clientID+".log", "Client: "+data+"\r\n",function(){});   
+      fs.appendFileSync("Client"+clientID+".log", "Server: ACK\r\n");    
       client.write('ACK');
     }else if(data == 'start test' || data == 'false' || data == 'true'){
+      fs.appendFileSync("Client"+clientID+".log", "Client: "+data + "\r\n");
+      fs.appendFileSync("Client"+clientID+".log", "Server: go\r\n");
       client.write('go');
     }else{
       // client.write('DEC');
+      fs.appendFileSync("Client"+clientID+".log", "Client: "+data + "\r\n");
       testArray.forEach(function(currentValue) {
       if(data == currentValue['question']){
         let index = Math.floor(Math.random() * 3);
         let a = testArray[index]['answer'];
         console.log('server answered: '+ a);
+        fs.appendFileSync("Client"+clientID+".log","Server: "+ a+"\r\n");
         client.write(a+ 'check');
       }
     });
     } 
   });
   client.on('close', () =>{
+    fs.appendFileSync("Client"+clientID+".log", "Client disconnected\r\n");
     console.log('Client disconnected');
+    clientID++;
   });
 });
 
