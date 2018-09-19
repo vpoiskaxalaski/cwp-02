c97d5fb582eb67f98d67b521b89c5d79109f6556
Alena <awff@mail.ru>
2018-09-19 22:48:00 +0300

    question/answer

--- client.js
+++ client.js
@@ -1,17 +1,18 @@
 // client.js
 const net = require('net');
 const fs = require('fs');
+var S = require('string');
 const port = 8124;
-
+let questionArray;
 const client = new net.Socket();
+let i = 0;
 
-client.setEncoding('utf8');
 
 //Перемешивание массива
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
@@ -19,26 +20,49 @@ function shuffleArray(array) {
 }
 
 client.connect(port, function() {
-  client.write('QA');
-  qaFile = fs.readFileSync('qa.json');  
-  let questions = JSON.parse(qaFile);
-  let questionArray = Array.from(questions);
-  shuffleArray(questionArray);
-  let obj = questionArray[3];
-  console.log(obj);
+	i=0;
+  	client.write('QA');
+  	qaFile = fs.readFileSync('qa.json');  
+  	let questions = JSON.parse(qaFile);
+  	questionArray = Array.from(questions); 
+	// shuffleArray(questionArray); 
 });
 
 
+function checkAnswer(ans, iterator){
+	if(S(ans).include(questionArray[iterator]['answer'])){
+			client.write('true');
+		}else{
+			client.write('false');
+		}
+}
 
-
-client.on('data', function(data) {
+client.on('data', (data) => {
+	//если серрвер подключил клиента
 	if(data == 'ACK'){
 		console.log('Connection is successful');
+		client.write('start test');		
 	}else if(data == 'DEC'){
 		console.log('Connection isn\'t successful');
+		client.destroy();
+	}else if(S(data).endsWith('check')){
+		questionArray.forEach(function(currentValue) {
+      		if(S(data).strip('check').s == currentValue['answer']){
+      		checkAnswer(data, i);
+			i++;
+      		}
+    	});
 	}
-
-	client.destroy();
+	else{
+		
+		//отправка вопросов
+		if(i==5){
+			client.destroy();
+		}else{
+			client.write(questionArray[i]['question']);
+		}
+	}
+	
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 // server.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
 let clientID = Date.now();
+
+//Перемешивание массива
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  return array;
+}
+
+const json =  fs.readFileSync('qa.json');
+let test = JSON.parse(json);
+let testArray = Array.from(test); 
+//shuffleArray(testArray);
 
 
 const server = net.createServer((client) => {
@@ -13,13 +30,22 @@ const server = net.createServer((client) => {
     console.log('client send: ' + data);
     if(data == 'QA'){      
       client.write('ACK');
+    }else if(data == 'start test' || data == 'false' || data == 'true'){
+      client.write('go');
     }else{
-      client.write('DEC');
+      // client.write('DEC');
+      testArray.forEach(function(currentValue) {
+      if(data == currentValue['question']){
+        let index = Math.floor(Math.random() * 3);
+        let a = testArray[index]['answer'];
+        console.log('server answered: '+ a);
+        client.write(a+ 'check');
+      }
+    });
     }
-  
+    
+    client.on('end', () => console.log('Client disconnected'));
   });
-
-  client.on('end', () => console.log('Client disconnected'));
 });
 
 server.listen(port, () => {
